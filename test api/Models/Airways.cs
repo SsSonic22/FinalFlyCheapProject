86a374dfe732b530086e19826986f3257b02dcd2
Arkadiy Goncharov <argon1989@ya.ru>
2023-09-05 00:27:33 +0300

    1

--- test api/Models/Airways.cs
+++ test api/Models/Airways.cs
@@ -1,28 +1,28 @@
-﻿namespace Test_api;
-
-public class Airways
+﻿namespace Test_api
 {
-    public List<FlightData> data { get; set; }
-    public string currency { get; set; }
-    public bool success { get; set; }
-}
+    public class Airways
+    {
+        public List<Data> data { get; set; }
+        public string currency { get; set; }
+        public bool success { get; set; }
+    }
 
-public class FlightData 
-{
-    public string flight_number { get; set; }
-    public string link { get; set; }
-    public string origin_airport { get; set; }
-    public string destination_airport { get; set; }
-    public DateTime departure_at { get; set; }
-    public string airline { get; set; }
-    public string destination { get; set; }
-    public DateTime return_at { get; set; }
-    public string origin { get; set; }
-    public int price { get; set; }
-    public int return_transfers { get; set; }
-    public int duration { get; set; }
-    public int duration_to { get; set; }
-    public int duration_back { get; set; }
-    public int transfers { get; set; }
+    public class Data
+    {
+        public string flight_number { get; set; }
+        public string link { get; set; }
+        public string origin_airport { get; set; }
+        public string destination_airport { get; set; }
+        public DateTime departure_at { get; set; }
+        public string airline { get; set; }
+        public string destination { get; set; }
+        public DateTime return_at { get; set; }
+        public string origin { get; set; }
+        public decimal price { get; set; }  // Обновленный тип данных
+        public decimal return_transfers { get; set; }  // Обновленный тип данных
+        public decimal duration { get; set; }  // Обновленный тип данных
+        public decimal duration_to { get; set; }  // Обновленный тип данных
+        public decimal duration_back { get; set; }  // Обновленный тип данных
+        public decimal transfers { get; set; }  // Обновленный тип данных
+    }
 }
-// Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
--- test api/Program.cs
+++ test api/Program.cs
@@ -1,34 +1,26 @@
 ﻿using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using Test_api.ApiManagers;
 
 namespace Test_api;
 
 class Program
 {
+    
+
     static async Task Main()
     {
-        // Укажите свой токен API
-        string apiToken = "bbaadb1a591224a0562c138a0a040e4e";
+        // ApiAviaSales _apiAviaSales = new ApiAviaSales();
+        Airways? airways = new Airways();
+        
+        var iata = _apiAviaSales.QueryIata(Console.ReadLine());
+        
+        
+    }
+}
 
-        // Параметры запроса
-        string currency = "usd"; // Валюта цен (по умолчанию - RUB)
-        string origin = "MAD"; // Код IATA города или аэропорта отправления
-        string destination = "BCN"; // Код IATA города или аэропорта назначения
-        string departureDate = "2023-07"; // Дата отправления (YYYY-MM или YYYY-MM-DD)
-        string returnDate = "2023-08"; // Дата возврата (не указывайте для билетов в один конец)
-        string directFlight = "false"; // Билеты без пересадок (true или false, по умолчанию: false)
-        string market = "us"; // Рынок источника данных (по умолчанию ru)
-        int limit = 30; // Количество записей на странице (по умолчанию 30, макс. 1000)
-        int page = 1; // Номер страницы (для пропуска результатов)
-        string sorting = "price"; // Сортировка цен (по цене или по популярности маршрута, по умолчанию: по цене)
-        string unique = "false"; // Возвращает только уникальные маршруты (если указан только origin, true или false, по умолчанию: false)
-
-        // URL для запроса
-        string url = $"https://api.travelpayouts.com/aviasales/v3/prices_for_dates?" +
-                     $"origin={origin}&destination={destination}&departure_at={departureDate}" +
-                     $"&return_at={returnDate}&unique={unique}&sorting={sorting}&direct={directFlight}" +
-                     $"&cy={currency}&limit={limit}&page={page}&one_way=true&token={apiToken}";
+/*
 
         using (HttpClient client = new HttpClient())
         {
@@ -40,17 +32,8 @@ class Program
                 {
                     string content = await response.Content.ReadAsStringAsync();
                     // Далее можете обрабатывать данные из ответа (content)
-                    Console.WriteLine(content);
-                    Airways? airways = JsonConvert.DeserializeObject<Airways>(content);
-                    Console.WriteLine(airways.success);
-                    int i = 0;
-                    foreach (var data in airways.data)
-                    {
-                        
-                        Console.WriteLine("итерация " + i++);
-                        Console.WriteLine(data.airline);
-                        Console.WriteLine(data.link);
-                    }
+                    //Console.WriteLine(content);
+                    airways = JsonConvert.DeserializeObject<Airways>(content);
                 }
                 else
                 {
@@ -61,13 +44,25 @@ class Program
             {
                 Console.WriteLine($"Произошла ошибка: {ex.Message}");
             }
-        }
-
-        
-    }
+        } 
+ 
+ 
+ 
+ 
+Console.WriteLine("airways.success ==> " + airways.success);
+var airwaysList = airways.data;
+Console.WriteLine("airwaysList.Count ==> " + airwaysList.Count);
+for (int i = 0; i < airwaysList.Count; i++)
+{
+    Console.WriteLine("итерация " + i);
+    Console.WriteLine(airways.data[i].airline);
+    //Console.WriteLine(airways.data[i].link);
 }
 
-/*
+
+
+
+
  Console.Write("Введите код страны (например, RU): ");
         string inputVar = Console.ReadLine().Trim().ToUpper();
 
@@ -97,10 +92,10 @@ class Program
                 Console.WriteLine("По вашему запросу ничего не найдено.");
             }
         }
- 
- 
- 
- 
+
+
+
+
  using (var context = new AirportContext())
         {
             // Получить все аэропорты
